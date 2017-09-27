0e99e2113ee63c09e4c8d82726f1ddb4e9567d34
vladimir <vladimir@pantelic.net>
2017-09-27 20:50:53 +0200

    update the av500 plugins to work with current VCV git
    Signed-off-by: vladimir <vladimir@pantelic.net>

--- src/av500.cpp
+++ src/av500.cpp
@@ -1,17 +1,14 @@
 #include "av500.hpp"
 
-struct av500Plugin : Plugin {
-	av500Plugin() {
-		slug = "av500";
-		name = "av500";
-		createModel<MultipleWidget>  (this, "av500 Multiple", 	"Triple Multiple");
-		createModel<Blank8hpWidget>  (this, "av500 8hp Blank", 	"8hp Blank");
-		createModel<p0wrWidget>      (this, "av500 p0wr", 	"p0wr");
-		createModel<TR808CowbellWidget>(this, "av500 808Cowbell", "808Cowbell");
-	}
-};
+Plugin *plugin;
 
-
-Plugin *init() {
-	return new av500Plugin();
+void init(rack::Plugin *p) {
+	plugin = p;
+	plugin->slug = "av500";
+	plugin->name = "av500";
+	createModel<MultipleWidget>(plugin, "av500 Multiple",    "Triple Multiple");
+	createModel<Blank8hpWidget>(plugin, "av500 8hp Blank",   "8hp Blank");
+	createModel<p0wrWidget>    (plugin, "av500 p0wr",        "p0wr");
+	createModel<TR808CowbellWidget>
+	                           (plugin, "av500 808Cowbell",  "808Cowbell");
 }
