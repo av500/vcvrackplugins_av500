c58b28a62da4c986433766e0259f90fa140ee88c
Vladimir Pantelic <vladimir@pantelic.net>
2017-11-21 16:20:30 +0100

    make it work with 0.5.0
    Signed-off-by: Vladimir Pantelic <vladimir@pantelic.net>

--- src/av500.cpp
+++ src/av500.cpp
@@ -8,10 +8,10 @@ void init(rack::Plugin *p) {
 #ifdef VERSION
 	p->version = TOSTRING(VERSION);
 #endif
-	p->addModel(createModel<MultipleWidget>    ("av500", "av500", "Multiple", "Multiple"));
-	p->addModel(createModel<Blank8hpWidget>    ("av500", "av500", "Blank", "8hp Blank"));
-	p->addModel(createModel<p0wrWidget>        ("av500", "av500", "Power", "p0wr"));
-	p->addModel(createModel<TR808CowbellWidget>("av500", "av500", "Drum", "808Cowbell"));
+	p->addModel(createModel<MultipleWidget>    ("av500", "Multiple", "Multiple", MULTIPLE_TAG));
+	p->addModel(createModel<Blank8hpWidget>    ("av500", "Blank", "8hp Blank", BLANK_TAG));
+	p->addModel(createModel<p0wrWidget>        ("av500", "Power", "p0wr", VISUAL_TAG));
+	p->addModel(createModel<TR808CowbellWidget>("av500", "Drum", "808Cowbell", DRUM_TAG));
 }
 
 //plugin->homepageUrl = "https://github.com/av500/vcvrackplugins_av500";
